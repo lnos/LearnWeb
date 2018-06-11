0905b7d67d309ee31b1ab0e914ec4ea0409b6307
Seong Kim <hoon5421@hotmail.com>
2018-06-10 23:06:51 -0400

    Reviewing Props/More ES6 Syntax

---
+++ LearnReact/src/components/video_list_item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+    
+    const imageURL = video.snippet.thumbnails.default.url;
+
+    //^video in curly braces is equivalent to bottom line
+    //const video = props.video;
+
+    return (
+        <li className = "list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageURL}/>
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
--- LearnReact/src/index.js
+++ LearnReact/src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCt_0NgE-UyEyPUcIIpWQVbjkstffTORAM';
 
-const App = () => {
-    return <div>
-        <SearchBar/>
-    </div>;
+//Most parent component should be responsible for fetching data
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {videos:[]};
+
+        YTSearch({key: API_KEY, term: 'penguins'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render(){
+        return( 
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
