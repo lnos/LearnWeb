db6480e6f0694e17cd996221b97515ecd9f7691b
Seong Kim <hoon5421@hotmail.com>
2018-12-27 17:31:39 -0500

    Reviewing Binding/Fetch

--- learnapi/src/components/App.js
+++ learnapi/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
-const App = () =>{
-    return (
-        <div className='ui container' style={{marginTop: '1.5rem'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component{
+ 
+    onSearchSubmit(term){
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers:{
+                Authorization: 'Client-ID a74843272610528434af31f3753639279b06fbe45837f037024a7ca9650d5ba6'
+            }
+        });
+    }
+
+    render(){
+        return (
+            <div className='ui container' style={{marginTop: '1.5rem'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    };
+}
 
 export default App;
